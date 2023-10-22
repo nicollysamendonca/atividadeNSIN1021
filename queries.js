62a9a32c2f6d18a2619a386e5a227002114c4559
Nicolly S. A. Mendonça <105677178+nicollysamendonca@users.noreply.github.com>
2023-10-22 18:24:52 +0000

    primeiro commit

---
+++ queries.js
@@ -0,0 +1,8 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'nkckfgcv',
+  host: 'isabelle.db.elephantsql.com',
+  database: 'nkckfgcv',
+  password: 'l9ocGJB1RveOoLzKiYTs50OElXlNy72Z',
+  port: 5432,
+})
