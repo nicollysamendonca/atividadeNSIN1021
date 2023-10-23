93beaed5f218616b19345b37c0f8288477bfbcd6
Nicolly S. A. Mendonça <105677178+nicollysamendonca@users.noreply.github.com>
2023-10-23 01:14:59 +0000

    quinto commit

--- index.js
+++ index.js
@@ -1,36 +1,63 @@
-const db = require('./queries')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const { Pool } = require('pg');
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+const pool = new Pool({
+  user: 'postgres',
+  host: 'aws-0-sa-east-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'AyrtonSenna@2003',
+  port: 6543,
+});
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+const getCurriculo = (request, response) => {
+  pool.query('SELECT * FROM curriculo ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+const createCurriculo = (request, response) => {
+  const { nome, email, telefone, cursos, formacao, endereco, outrosContatos } = request.body;
 
-app.get('/curriculo', db.getCurriculo)
-app.post('/curriculo', db.createCurriculo)
-app.put('/curriculo/:id', db.updateCurriculo)
-app.delete('/curriculo/:id', db.deleteCurriculo)
+  pool.query('INSERT INTO curriculo (nome, email, telefone, cursos, formacao, endereco, outrosContatos) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *', [nome, email, telefone, cursos, formacao, endereco, outrosContatos], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+  });
+};
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+const updateCurriculo = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { nome, email, telefone, cursos, formacao, endereco, outrosContatos } = request.body;
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+  pool.query(
+    'UPDATE curriculo SET nome = $1, email = $2, telefone = $3, cursos = $4, formacao = $5, endereco = $6, outrosContatos = $7 WHERE id = $8',
+    [nome, email, telefone, cursos, formacao, endereco, outrosContatos, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteCurriculo = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query('DELETE FROM curriculo WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getCurriculo,
+  createCurriculo,
+  updateCurriculo,
+  deleteCurriculo,
+};
